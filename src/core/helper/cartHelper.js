1a3d40a238287c5a34505b295a48ff3cfaaf6b7c
Aakash <tiwaryaakash00@gmail.com>
2022-12-24 13:38:46 +0530

    dc

--- src/core/helper/cartHelper.js
+++ src/core/helper/cartHelper.js
@@ -8,18 +8,12 @@ export const addItemToCart = async(item,cartItems,setCartItems,_id)=>{
   if(_id===undefined)
          return toast.warn("please Signin to continue");
         
-         console.log("cartHelper")
-         console.log(cartItems);
-        
+       
         const num = cartItems.filter(obj=>obj.product._id===item._id);
-        // console.log("num here")
-        // console.log(num);
+        
         if(num.length>0 && num[0].quantity===num[0].product.stock)
          return toast.info('Sorry our stock for this product is over');
 
-      //Todo: make a call here to push the object in array 
-      //if there is same product in array the quantity of that must be updated and if there is no such 
-      // product then item must be pushed with quantity 1
     try{  
 
       let object = {
@@ -39,16 +33,15 @@ export const addItemToCart = async(item,cartItems,setCartItems,_id)=>{
 
     const response = await middle.json();
      
-    console.log("response from cartHelper")
-    console.log(response);
+    
     setCartItems(response.cart);
 
-    // console.log(response);
+    
   }
   catch(error){
      console.log("error");
   }  
-      // console.log(item);
+      
 } 
 
 
@@ -58,7 +51,7 @@ export const addItemToCart = async(item,cartItems,setCartItems,_id)=>{
 export const removeItemFromCart = async(productId,setCartItems,userId)=>{
    
 
-       console.log(userId);
+      
 
        if(userId===undefined)
          return toast.warn("please Signin to continue");
@@ -75,8 +68,7 @@ export const removeItemFromCart = async(productId,setCartItems,userId)=>{
 
         const response  = await middle.json();
         
-        console.log("remove from cart response");
-        console.log(response)
+        
         setCartItems(response.cart);
 
       }
